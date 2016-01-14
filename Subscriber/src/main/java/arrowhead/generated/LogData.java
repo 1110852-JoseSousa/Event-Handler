e0d633f6f9c4fd96968f4d56a2fb53adc402d456
José Sousa <1110852@isep.ipp.pt>
2016-01-14 11:22:09 +0000

    #2 Create the new LogData class which will contain all logged data elements.

---
+++ Subscriber/src/main/java/arrowhead/generated/LogData.java
@@ -0,0 +1,107 @@
+package arrowhead.generated;
+
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for LogData complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="LogData">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="event" type="{http://www.arrowhead.org/eventhandler/events}EventType" maxOccurs="1" minOccurs="0"/>
+ *         &lt;element name="consumer" type="{http://www.arrowhead.org/eventhandler/registered}ConsumerType" maxOccurs="unbounded" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "LogData", propOrder = {
+    "event",
+    "consumerList"
+})
+
+@XmlRootElement(name = "LogData")
+public class LogData {
+
+    protected EventType event;
+    protected List<ConsumerType> consumerList;
+
+    /**
+     * Gets the event.
+     * 
+     * @return
+     *     possible object is
+     *     {@link EventType }
+     *     
+     */
+    public EventType getEvent() {
+        return event;
+    }
+
+    /**
+     * Sets the event properties, including .
+     * 
+     *     
+     * @param from
+     * allowed object is
+     *     {@link String }
+     * @param type
+     * allowed object is
+     *     {@link String }
+     * @param description
+     * allowed object is
+     *     {@link Meta }
+     * @param payload
+     * allowed object is
+     *     {@link String }
+     */
+    public void setEvent(String from, String type, Meta description, String payload) {
+        this.event.setFrom(from);
+        this.event.setType(type);
+        this.event.setDescription(description);
+        this.event.setPayload(payload);
+    }
+
+    /**
+     * Sets the list of consumers that were notified.
+     * 
+     * @param listC
+     * allowed object is
+     *     {@link List<ConsumerType> }
+     * 
+     */
+    public void setConsumers(List<ConsumerType> listC) {
+        this.consumerList.clear();
+        for (ConsumerType c : listC){
+            this.consumerList.add(c);
+        }
+    }
+   
+    /**
+     * Gets the list of notified subscribers.
+     * 
+     * @return
+     *     possible object is
+     *     {@link List<ConsumerType> }
+     *     
+     */
+    public List<ConsumerType> getListConsumers() {
+        return this.consumerList;
+    }
+    
+}
